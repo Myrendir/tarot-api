3dfe3100e54c3581cbaf97a1b6a606528521a2ff
armand.deshais <armand.deshais@siqual.fr>
2023-08-23 10:25:20 +0200

    feat(app): ajoute le middleware cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const db = require('./db'); // Importez le nouveau fichier db.js
 
 const playerRoutes = require('./src/routes/playerRoute');
@@ -8,7 +9,7 @@ const securityRoutes = require('./src/routes/securityRoute');
 const sessionRoutes = require('./src/routes/sessionRoute');
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
