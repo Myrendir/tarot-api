2ea7b0b2b9c3fe8928ae061a03035f12b42a6be0
armand.deshais <armand.deshais@siqual.fr>
2023-08-23 09:43:32 +0200

    feat(database): ajoute la connection à mongodb

---
+++ db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const mongoString = `${process.env.DATABASE_URL}dev_tarot`;
+
+const database = mongoose.connection;
+
+database.on('error', (error) => {
+    console.log(error);
+});
+
+database.once('connected', () => {
+    console.log('Database Connected');
+});
+
+// Connect to the database
+mongoose.connect(mongoString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+module.exports = database;
